0e026cf37cca4ea8fe8ec7097f092b13e15e9cc9
Nattida Wong <nattidac.wong@gmail.com>
2020-09-11 13:29:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,7 @@
 def speak_to_grandma(speak)
   if speak == "Hi Nana, how are you?"
     "huh?! speak up, sonny!".upcase
+  elsif speak == "Hi!"  
   else speak == "i love you grandma!".upcase
     "i love you too pumpkin!".upcase
   
